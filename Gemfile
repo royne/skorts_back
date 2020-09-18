ae94c0c27388f6f355e0238cb58576d1228eb31f
royne <royne.chaves@gmail.com>
2020-09-18 17:09:50 -0500

    add jwt with knock

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'puma', '~> 3.11'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
@@ -26,11 +26,16 @@ gem 'puma', '~> 3.11'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem 'rack-cors'
+gem 'rack-cors'
+gem 'knock'
+gem 'active_model_serializers', '~> 0.10.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry'
+  gem 'pry-remote'
+  gem 'pry-nav'
 end
 
 group :development do
---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  # Con este método encriptamos al password y generamos el método "authenticate"
+  # que será usado por Knock
+  has_secure_password
+
+  before_save   :downcase_email, :downcase_username
+
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z]+[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :username,  presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 255 },
+  format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password,  presence: true, length: { minimum: 6, maximum: 50 }, allow_nil: true
+
+  private
+
+    # Se encarga de que email tenga sólo letras minúsculas
+    def downcase_email
+      self.email.downcase!
+    end
+
+    def downcase_username
+      self.username.downcase!
+    end
+end
